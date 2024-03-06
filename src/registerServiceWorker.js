41e18e9de3f2610b87a80d80cba74fcd3c249fe3
Victory Nwani <vickywane@gmail.com>
2024-03-06 13:38:34 +0000

    feat: implement service-worker with Workbox

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,9 +1,30 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/serviceWorker.js').then(registration => {
-        console.log('Service worker registered:', registration);
-      }).catch(error => {
-        console.error('Service worker registration failed:', error);
-      });
-    }); 
-  }
+import { register } from "register-service-worker";
+
+register(`${process.env.BASE_URL}service-worker.js`, {
+  ready() {
+    console.log(
+      "App is being served from cache by a service worker.\n" +
+        "For more details, visit https://goo.gl/AFskqB"
+    );
+  },
+  registered() {
+    console.log("Service worker has been registered.");
+  },
+  cached() {
+    console.log("Content has been cached for offline use.");
+  },
+  updatefound() {
+    console.log("New content is downloading.");
+  },
+  updated() {
+    console.log("New content is available; please refresh.");
+  },
+  offline() {
+    console.log(
+      "No internet connection found. App is running in offline mode."
+    );
+  },
+  error(error) {
+    console.error("Error during service worker registration:", error);
+  },
+});
