7a8900b375d31463039db2f8bba6c4389d73effc
Victory Nwani <vickywane@gmail.com>
2024-03-06 15:28:33 +0000

    feat: implement service-worker with Workbox

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.997b9vfc2h8"
+    "revision": "0.meagqilrloo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,38 @@
-import { fileURLToPath, URL } from 'node:url'
-import path from 'path'
-import { VitePWA } from 'vite-plugin-pwa'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { fileURLToPath, URL } from "node:url";
+import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: path.join(__dirname, "docs")
+    outDir: path.join(__dirname, "docs"),
   },
   plugins: [
     vue(),
     vueJsx(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: "autoUpdate",
       injectRegister: "auto",
       devOptions: {
-        enabled: true
-      }
-    })
+        enabled: true,
+      },
+      manifest: {
+        name: "Coursely PWA App",
+        short_name: "Coursely Vue",
+        theme_color: "#FFF8F0",
+        background_color: "#000000",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+      },
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
