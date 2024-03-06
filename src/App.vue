5deef2809e270f0224cded59a874144d09f1c276
Victory Nwani <vickywane@gmail.com>
2024-03-06 13:11:24 +0000

    feat: implement PWA installatiin in App.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,54 @@
-<script setup lang="ts">
+<script setup>
+import { onBeforeMount, ref } from "vue"
 import HomeView from "./views/HomeView.vue";
+
+let pwaInstallEvent;
+
+const state = ref({
+  pwaSupportAvailable: false
+})
+
+onBeforeMount(() => {
+  window.addEventListener('beforeinstallprompt', (e) => {
+    console.log("PWA SUPPORT DETECTED!");
+    state.pwaSupportAvailable = true
+
+    e.preventDefault();
+    pwaInstallEvent = e;
+  })
+})
+
+const pwaInstall = async () => {
+  pwaInstallEvent.prompt()
+  const { outcome } = await pwaInstallEvent.userChoice;
+  if (outcome === 'accepted') {
+    console.log("PWA INSTALLED!");
+
+    // Take extra actions if the user opted for installation.
+  } else {
+    console.log("PWA NOT INSTALLED!");
+
+    // Take extra actions if the user cancels.
+  }
+}
 </script>
 
 <template>
   <HomeView />
+
+  <div v-if="state.pwaSupportAvailable" class="flex w-full absolute bottom-10 justify-center">
+    <div class="flex items-center h-[60px] rounded-lg px-12 bg-blue-500 shadow-lg">
+      <div class="mr-12">
+        <p class="text-xl text-white">Coursely App supports offline use. Download Now!</p>
+      </div>
+
+      <div>
+        <button @click="pwaInstall" class="bg-white text-blue-500 py-2 px-8 rounded">
+          Download
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
